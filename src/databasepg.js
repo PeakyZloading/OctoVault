054b97759710b34f00dc23ac4f198305fd41be98
Miguel Zepeda <miguelzepeda350@gmail.com>
2023-09-17 22:14:11 -0400

    Slight change to main page with greeting

---
+++ src/databasepg.js
@@ -0,0 +1,23 @@
+// // databasepg.js
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   host: "localhost",
+//   user: "postgres",
+//   port: 5432,
+//   password: "rootuser",
+//   database: "postgres"
+// });
+
+// client.connect();
+
+
+function logInput(input) {
+    // Log the passed-in input
+    console.log("User input:", input);
+}  
+
+console.log("we are here")
+
+
+// module.exports = { client };
