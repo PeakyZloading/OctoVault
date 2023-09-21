40e1bb13362b9f9073d1a8c4eff855bc1346a2b6
Miguel Zepeda <miguelzepeda350@gmail.com>
2023-09-20 21:45:34 -0400

    Adds overview page and routing to it

--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/overview', methods=['GET', 'POST'])
+def overview():
+    return render_template("overview.html")
