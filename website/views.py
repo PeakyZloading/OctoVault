1adbdfc1be2ef7db2cbfc22b082c80eef03b90e4
Miguel Zepeda <miguelzepeda350@gmail.com>
2023-09-24 19:22:19 -0400

    Implements connection to sqlite db and implements half of the fighter overview page with styling

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request
+import sqlite3
+import os
 
 views = Blueprint('views', __name__)
 
@@ -8,8 +10,49 @@ def home():
 
 @views.route('/overview', methods=['GET'])
 def overview():
-    data = request.args
     fighter_name = request.args.get('octo_input')
-    print(fighter_name)
 
-    return render_template("overview.html")
+    # Determine the full path to the desktop directory
+    desktop_path = os.path.expanduser("~/Desktop")
+    db_file_path = os.path.join(desktop_path, "octovaultdb")
+
+    # Establish a connection to the SQLite database using the full path
+    conn = sqlite3.connect(db_file_path)
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM fighters WHERE LOWER(first_name) = ? AND LOWER(last_name) = ?"
+    first_name, last_name = fighter_name.split()
+    first_name = first_name.lower()
+    last_name = last_name.lower()
+    params = (first_name, last_name)
+
+    cursor.execute(query, params)
+    fighter_info = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    #Following block handles if a fighter is typed in
+    if fighter_info:
+        fighter_data = {
+            'fighter_name': f"{fighter_info[0][0]} {fighter_info[0][1]}",
+            'fighter_nickname': fighter_info[0][2],
+            'wins': fighter_info[0][7],
+            'losses': fighter_info[0][8],  
+            'draws': fighter_info[0][9],  
+            'height': fighter_info[0][3],
+            'weight': fighter_info[0][4],
+            'reach': fighter_info[0][5],
+            'stance': fighter_info[0][6],
+        }
+    else:
+        #Handle the case where the fighter is not found
+        fighter_data = {
+            'fighter_name': 'Fighter Not Found',
+            'wins': 0,
+            'losses': 0,
+            'draws': 0,
+            'previous_fights': []
+        }
+
+    return render_template("overview.html", **fighter_data)
