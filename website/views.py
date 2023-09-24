d48925f86e4b2b9182736c44680853565a8c40ed
Miguel Zepeda <miguelzepeda350@gmail.com>
2023-09-24 00:24:40 -0400

    Adds basic get request handling for main form

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
@@ -6,6 +6,10 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 
-@views.route('/overview', methods=['GET', 'POST'])
+@views.route('/overview', methods=['GET'])
 def overview():
+    data = request.args
+    fighter_name = request.args.get('octo_input')
+    print(fighter_name)
+
     return render_template("overview.html")
