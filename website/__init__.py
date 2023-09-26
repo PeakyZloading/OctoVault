b5ef0b3d0f1372d9d515fefb95b7b1bc3304c197
Miguel Zepeda <miguelzepeda350@gmail.com>
2023-09-26 12:27:27 -0400

    Fixes security bug

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from .config import SECRET_KEY
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'fewfefbfbe123ewqw_99'
+    app.config['SECRET_KEY'] = SECRET_KEY
 
     from .views import views
 
